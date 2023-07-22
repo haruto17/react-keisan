62b1f4ff5c54e9c503c5ac0723f6d182f820d74a
haruto17 <s1902272@sendai-nct.jp>
2023-07-22 23:33:03 +0900

    レイアウトの修正

--- src/App.tsx
+++ src/App.tsx
@@ -113,79 +113,49 @@ function App() {
   return (
     <div className="App">
       <FlexGrid
-        flexGridColumnCount={2}
+        flexGridColumnCount={1}
         flexGridRowGap="scale400"
         flexGridColumnGap="scale200"
       >
-        <FlexGridItem
-          className={css({
-            maxWidth: "fit-content",
-          })}
-        >
+        <FlexGridItem>
           <ButtonGroup>
             <Button onClick={() => handleMemButtonClick()}>M+</Button>
             <Button>M-</Button>
             <Button onClick={() => handleOpeButtonClick("%")}>%</Button>
+            <Button onClick={() => handleClearButtonClick()}>C</Button>
           </ButtonGroup>
         </FlexGridItem>
         <FlexGridItem>
-          <Button onClick={() => handleClearButtonClick()}>C</Button>
-        </FlexGridItem>
-        <FlexGridItem
-          className={css({
-            maxWidth: "fit-content",
-          })}
-        >
           <ButtonGroup>
             <Button onClick={() => handleButtonClick("7")}>7</Button>
             <Button onClick={() => handleButtonClick("8")}>8</Button>
             <Button onClick={() => handleButtonClick("9")}>9</Button>
+            <Button onClick={() => handleOpeButtonClick("/")}>/</Button>
           </ButtonGroup>
         </FlexGridItem>
         <FlexGridItem>
-          <Button onClick={() => handleOpeButtonClick("/")}>/</Button>
-        </FlexGridItem>
-        <FlexGridItem
-          className={css({
-            maxWidth: "fit-content",
-          })}
-        >
           <ButtonGroup>
             <Button onClick={() => handleButtonClick("4")}>4</Button>
             <Button onClick={() => handleButtonClick("5")}>5</Button>
             <Button onClick={() => handleButtonClick("6")}>6</Button>
+            <Button onClick={() => handleOpeButtonClick("*")}>*</Button>
           </ButtonGroup>
         </FlexGridItem>
         <FlexGridItem>
-          <Button onClick={() => handleOpeButtonClick("*")}>*</Button>
-        </FlexGridItem>
-        <FlexGridItem
-          className={css({
-            maxWidth: "fit-content",
-          })}
-        >
           <ButtonGroup>
             <Button onClick={() => handleButtonClick("1")}>1</Button>
             <Button onClick={() => handleButtonClick("2")}>2</Button>
             <Button onClick={() => handleButtonClick("3")}>3</Button>
+            <Button onClick={() => handleOpeButtonClick("-")}>-</Button>
           </ButtonGroup>
         </FlexGridItem>
         <FlexGridItem>
-          <Button onClick={() => handleOpeButtonClick("-")}>-</Button>
-        </FlexGridItem>
-        <FlexGridItem
-          className={css({
-            maxWidth: "fit-content",
-          })}
-        >
           <ButtonGroup>
             <Button onClick={() => handleButtonClick("0")}>0</Button>
             <Button onClick={() => handleCommaClick()}>.</Button>
             <Button onClick={() => handleEqualButtonClick()}>=</Button>
+            <Button onClick={() => handleOpeButtonClick("+")}>+</Button>
           </ButtonGroup>
-        </FlexGridItem>
-        <FlexGridItem>
-          <Button onClick={() => handleOpeButtonClick("+")}>+</Button>
         </FlexGridItem>
       </FlexGrid>
       <div>
